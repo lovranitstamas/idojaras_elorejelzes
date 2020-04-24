05cab391d4500c20fc1dcf4dcdc5793977a4e295
lovranitslaptop <tamas198601@gmail.com>
2020-04-24 13:53:09 +0200

    Test the indexedDB with city.list.json

--- src/app/search/search.component.ts
+++ src/app/search/search.component.ts
@@ -3,21 +3,17 @@ import {ActivatedRoute, Router} from '@angular/router';
 import {WeatherMapService} from '../shared/weather-map.service';
 import {HttpClient} from '@angular/common/http';
 
-// import example from './assets/data/example.json';
-
 @Component({
   selector: 'app-search',
   templateUrl: './search.component.html',
   styleUrls: ['./search.component.scss']
 })
 export class SearchComponent implements OnInit {
-  
+
   query: string;
   results: any;
 
-  // http://bulk.openweathermap.org/sample/
   private _jsonURL = './assets/data/city.list.json';
-  private _jsonURL2 = './assets/data/example.json';
 
   constructor(private _weather: WeatherMapService,
               private _router: Router,
@@ -64,37 +60,25 @@ export class SearchComponent implements OnInit {
 
     // let apiURL = `${this.apiRoot}?term=${term}&media=music&limit=20`;
     this._http.get(this._jsonURL).subscribe((data) => {
-      console.log(data);
+
+      console.log(Object.values(data));
       const dbReq = indexedDB.open('weatherDB', 1);
-      // https://developer.mozilla.org/en-US/docs/Web/API/IDBTransaction/onerror
+
       dbReq.onupgradeneeded = (event) => {
         // Set the db variable to our database so we can use it!
         const db = (event.target as IDBOpenDBRequest).result;
-        console.log(db);
+
         // Create an object store named notes. Object stores
         // in databases are where data are stored.
-        const objectStore = db.createObjectStore('customers', {keyPath: 'ssn'});
-
-        // Create an index to search customers by name. We may have duplicates
-        // so we can't use a unique index.
-        // objectStore.createIndex('name', 'name', {unique: false});
-
-        // Create an index to search customers by email. We want to ensure that
-        // no two customers have the same email, so use a unique index.
-        // objectStore.createIndex('email', 'email', {unique: true});
-
-        const customerData = [
-          {ssn: '444-44-4444', name: 'Bill', age: 35, email: 'bill@company.com'},
-          {ssn: '555-55-5555', name: 'Donna', age: 32, email: 'donna@home.org'}
-        ];
+        const objectStore = db.createObjectStore('cities', {keyPath: 'id'});
 
         // Use transaction oncomplete to make sure the objectStore creation is
         // finished before adding data into it.
         objectStore.transaction.oncomplete = () => {
           // Store values in the newly created objectStore.
-          const customerObjectStore = db.transaction('customers', 'readwrite').objectStore('customers');
-          customerData.forEach((customer) => {
-            customerObjectStore.add(customer);
+          const customerObjectStore = db.transaction('cities', 'readwrite').objectStore('cities');
+          Object.values(data).forEach((city) => {
+            customerObjectStore.add(city);
           });
           console.log(db);
         };
@@ -107,17 +91,17 @@ export class SearchComponent implements OnInit {
     dbReq.onsuccess = (event) => {
       const db = (event.target as IDBOpenDBRequest).result;
 
-      const customerData = [
-        {ssn: '5545-55-5555', name: 'Donnda', age: 32, email: 'tamas76@home.org'}
+      const cityData = [
+        {sn: '5545-55-5555', name: 'Donnda', age: 32, email: 'tamas76@home.org'}
       ];
 
       // open a read/write db transaction, ready for adding the data
-      const transaction = db.transaction(['customers'], 'readwrite');
+      const transaction = db.transaction(['cities'], 'readwrite');
 
       // create an object store on the transaction
-      const objectStore = transaction.objectStore('customers');
+      const objectStore = transaction.objectStore('cities');
       // add our newItem object to the object store
-      const objectStoreRequest = objectStore.add(customerData[0]);
+      const objectStoreRequest = objectStore.add(cityData[0]);
 
       objectStoreRequest.onsuccess = () => {
         // report the success of the request (this does not mean the item
